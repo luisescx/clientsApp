4fad0c998cee358723f650edfe18855873f9c366
luisescx <luis.esculapio@live.com>
2022-04-24 23:56:53 -0300

    feat: added navigation

---
+++ src/navigations/AppNavigation/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { PersonList } from '~/scenes';
+import PersonForm from '~/scenes/PersonForm';
+
+const { Navigator, Group, Screen } = createNativeStackNavigator();
+
+const AppNavigation = () => {
+  return (
+    <Navigator
+      initialRouteName="PersonList"
+      screenOptions={{ headerShown: false }}
+    >
+      <Screen name="PersonList" component={PersonList} />
+
+      <Group
+        screenOptions={{
+          presentation: 'fullScreenModal',
+          headerShown: true,
+        }}
+      >
+        <Screen
+          name="PersonForm"
+          component={PersonForm}
+          options={{
+            title: 'Novo Colaborador',
+          }}
+        />
+      </Group>
+    </Navigator>
+  );
+};
+
+export default AppNavigation;
