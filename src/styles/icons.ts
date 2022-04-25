bffd69953e9ad8d43828c7fc23d078a062449b63
luisescx <luis.esculapio@live.com>
2022-04-25 13:37:19 -0300

    .

--- src/styles/icons.ts
+++ src/styles/icons.ts
@@ -1,11 +1,15 @@
 import Pencil from '~/assets/icons/pencil.svg';
 import Plus from '~/assets/icons/plus.svg';
+import Reload from '~/assets/icons/reload.svg';
+import Search from '~/assets/icons/search.svg';
 import Trash from '~/assets/icons/trash.svg';
 
 const icons = {
   pencil: Pencil,
   trash: Trash,
   plus: Plus,
+  search: Search,
+  reload: Reload,
 };
 
 export default icons;
