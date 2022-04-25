18fbe7583dcd36bcd38ba7430e45d90b9c7f7520
luisescx <luis.esculapio@live.com>
2022-04-25 08:56:13 -0300

    feat: added new function on utils

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -2,10 +2,14 @@ export function formatDateToString(date: Date) {
   return new Intl.DateTimeFormat('br-PT').format(new Date(date));
 }
 
-export function parseStringToDate(stringDate: string) {
+export function parseStringDateFormat(stringDate: string) {
   const day = stringDate.split('/')[0];
   const month = stringDate.split('/')[1];
   const year = stringDate.split('/')[2];
 
   return `${year}-${`0${month}`.slice(-2)}-${`0${day}`.slice(-2)}`;
+}
+
+export function parseStringToDate(stringDate: string) {
+  return new Date(parseStringDateFormat(stringDate));
 }
